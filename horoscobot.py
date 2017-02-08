aa81a4afbb15da09e1b196268047edde2d153a78
Mauricio Quezada <waxkun@gmail.com>
2017-02-07 21:55:42 -0300

    first commit

---
+++ horoscobot.py
@@ -0,0 +1,54 @@
+# -*- encoding: utf-8 -*-
+
+import telepot
+from pprint import pprint
+import time
+import random
+import string
+import sys
+
+signos = ['aries',
+		  'tauro',
+		  'geminis',
+		  'cancer',
+		  'acuario',
+		  'piscis',
+		  'leo',
+		  'virgo',
+		  'libra',
+		  'escorpio',
+		  'sagitario',
+		  'capricornio']
+
+predicciones = dict()
+for signo in signos:
+	with open('signos/' + signo + '.txt') as f:
+		predicciones[signo] = f.readlines()
+
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    flavor = telepot.flavor(msg)
+    pprint(msg)
+    pprint("flavor " + flavor)
+
+    if flavor != 'chat':
+        return
+
+    if 'text' in msg:
+        text = msg['text']
+        signo = text[1:].split("@")[0]
+        if signo in signos:
+            bot.sendChatAction(chat_id, "typing")
+            #time.sleep(3)
+            m = "*%s*\n%s" % (signo.capitalize().decode('utf-8'), predicciones[signo][0].decode('utf-8'))
+            bot.sendMessage(chat_id, m, parse_mode="Markdown")
+
+token = sys.argv[1]
+bot = telepot.Bot(token)
+
+print 'Listening ...'
+bot.message_loop(handle)
+
+while 1:
+    time.sleep(10)
